5d58b1c9fc0b7fb3a66003a8f2f9e33c9b714120
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-05-20 17:27:20 -0300

    :cookie: User login with GitHub in Mobile app. Storage the access Token and Redirecting with Expo-Router

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function memories() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Memories</Text>
+    </View>
+  )
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,7 @@ app.register(jwt, {
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('SERVER IS RUNNING 👌')
